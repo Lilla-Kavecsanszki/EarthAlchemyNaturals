1f111d050a45b08e67ad2fdfbfea2c28826a2d1e
Lilla-Kavecsanszki <>
2023-09-05 14:18:55 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 from .models import Category, SkinType, Product
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(SkinType)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'member_price',
+        'skin_types',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SkinTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SkinType, SkinTypeAdmin)
--- products/models.py
+++ products/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Category model
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
@@ -32,7 +36,7 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     skin_types = models.ManyToManyField('SkinType')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)    
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
