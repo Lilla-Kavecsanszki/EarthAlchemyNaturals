635039a54cafdb72852edcdf2386bd66c4d21eb9
Lilla-Kavecsanszki <>
2023-09-09 10:34:18 +0000

    Added sorting tab JS and fixed info button to the product page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category, SkinType
+from django.db.models.functions import Lower
 
 # Create your views here.
 
