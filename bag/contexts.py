81f861d31c6e2ba37441e7b6d8ace8b532f37006
Lilla-Kavecsanszki <>
2023-09-16 15:10:44 +0000

    Removed codes logic for optional personalisation

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,37 +3,22 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-# delivery cost calculation
-
-
+# Delivery cost calculation
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, item_data in bag.items():
-        if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for personal, quantity in item_data['items_by_personal'].items():
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'personal': personal,
-                })
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -41,9 +26,7 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
     grand_total = delivery + total
-
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -53,5 +36,4 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
     return context
--- bag/views.py
+++ bag/views.py
@@ -2,82 +2,47 @@ from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
-
 def view_bag(request):
     """ A view that renders the bag contents page """
-
     return render(request, 'bag/bag.html')
-
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    personal = None
-    if 'product_personal' in request.POST:
-        personal = request.POST['product_personal']
     bag = request.session.get('bag', {})
 
-    if personal:
-        if item_id in list(bag.keys()):
-            if personal in bag[item_id]['items_by_personal'].keys():
-                bag[item_id]['items_by_personal'][personal] += quantity
-            else:
-                bag[item_id]['items_by_personal'][personal] = quantity
-        else:
-            bag[item_id] = {'items_by_personal': {personal: quantity}}
+    if item_id in bag:
+        bag[item_id] += quantity
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
-            bag[item_id] = quantity
+        bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
-
 def adjust_bag(request, item_id):
-    """Adjust the quantity of the specified product to the specified amount"""
+    """ Adjust the quantity of the specified product to the specified amount """
 
     quantity = int(request.POST.get('quantity'))
-    personal = None
-    if 'product_personal' in request.POST:
-        personal = request.POST['product_personal']
     bag = request.session.get('bag', {})
 
-    if personal:
-        if quantity > 0:
-            bag[item_id]['items_by_personal'][personal] = quantity
-        else:
-            del bag[item_id]['items_by_personal'][personal]
-            if not bag[item_id]['items_by_personal']:
-                bag.pop(item_id)
+    if quantity > 0:
+        bag[item_id] = quantity
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-        else:
-            bag.pop(item_id)
+        bag.pop(item_id, None)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
-
 def remove_from_bag(request, item_id):
-    """Remove the item from the shopping bag"""
+    """ Remove the item from the shopping bag """
 
     try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-        else:
-            bag.pop(item_id)
+        if item_id in bag:
+            bag.pop(item_id, None)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
