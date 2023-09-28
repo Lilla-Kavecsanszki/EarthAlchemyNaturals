8a797ed13d61420020bce467b438aca48426369a
Lilla-Kavecsanszki <>
2023-09-28 10:30:57 +0000

    Fixed the issue that blocked images to upload, saved the widget

--- products/views.py
+++ products/views.py
@@ -93,6 +93,7 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
+            # print('form data: ', form.cleaned_data)
             product = form.save()
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
