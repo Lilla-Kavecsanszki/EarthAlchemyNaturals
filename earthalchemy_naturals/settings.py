b99c105498b38830df858ff9471b4ee046fec03d
Lilla-Kavecsanszki <>
2023-10-10 17:27:32 +0000

    Turn Debug

--- earthalchemy_naturals/settings.py
+++ earthalchemy_naturals/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['earthalchemy-naturals.herokuapp.com',
                  'localhost', 'earthalchemy-naturals-99139eee523b.herokuapp.com']
