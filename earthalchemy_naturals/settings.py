f2819fa7569f96d38602403c8dcb9de63c6f7df6
Lilla-Kavecsanszki <>
2023-10-10 15:51:38 +0000

    Deployment commit

--- earthalchemy_naturals/settings.py
+++ earthalchemy_naturals/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 if os.path.exists("env.py"):
     import env
+import dj_database_url
 
 from pathlib import Path
 
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-lilla-kavecsanszki-earth-1sbn79wqnr.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['earthalchemy-naturals.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -126,13 +127,17 @@ WSGI_APPLICATION = 'earthalchemy_naturals.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
