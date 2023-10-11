709d3894065df81a8d68f2ea8908ae1f7fd690ca
Lilla-Kavecsanszki <>
2023-10-11 12:05:26 +0000

    Added cache control

--- earthalchemy_naturals/settings.py
+++ earthalchemy_naturals/settings.py
@@ -188,6 +188,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+     
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'earthalchemy-naturals'
     AWS_S3_REGION_NAME = 'eu-west-2'
